6d1affd5a78d1f34e7f93c80d519812b4c69ba9c
Nusvara <yusufusman11111111@gmail.com>
2026-08-28 10:18:13 +0700

    feat: menambahkan endpoints customer

--- app/Modules/Customer/Controllers/CustomerController.php
+++ app/Modules/Customer/Controllers/CustomerController.php
@@ -8,4 +8,19 @@ class CustomerController
     {
         return response()->json(['message' => 'Customer Controller']);
     }
+
+    public function create()
+    {
+        return response()->json(['message' => 'Customer created']);
+    }
+
+    public function edit($id)
+    {
+        return response()->json(['message' => 'Customer updated']);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json(['message' => 'Customer deleted']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Modules\Auth\Controllers\AuthController;
+use App\Modules\Customer\Controllers\CustomerController;
 
 // Auth Endpoints
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/user', [AuthController::class, 'user'])->name('user');
+
+// Customer Endpoints
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+Route::post('/customers', [CustomerController::class, 'create'])->name('customers.create');
+Route::put('/customers/{id}', [CustomerController::class, 'edit'])->name('customers.edit');
+Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('customers.destroy');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
