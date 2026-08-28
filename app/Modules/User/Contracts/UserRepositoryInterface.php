0fcab703e569391e3f43fb28e24b2d3245070cc7
Nusvara <yusufusman11111111@gmail.com>
2026-08-28 10:18:51 +0700

    refactor: menghapus module user, dan memutuskan untuk gabung dengan module auth

--- app/Modules/User/Contracts/UserRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Modules\User\Contracts;
-
-use App\Infrastructure\Contracts\BaseRepositoryInterface;
-
-interface UserRepositoryInterface extends BaseRepositoryInterface
-{
-}
